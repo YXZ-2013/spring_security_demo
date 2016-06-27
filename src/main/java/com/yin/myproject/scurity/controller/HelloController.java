f080428ac5ffcb609025621dfb94881778b631d5
yinxunzhi <yin380697242@163.com>
2016-06-27 22:58:59 +0800

    修改一些文件  打tag0.0.1-framework

--- src/main/java/com/yin/myproject/scurity/controller/HelloController.java
+++ src/main/java/com/yin/myproject/scurity/controller/HelloController.java
@@ -1,21 +1,7 @@
 package com.yin.myproject.scurity.controller;
  
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
  
 @Controller
 public class HelloController {
- 
-	@RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
-	public ModelAndView welcomePage() {
-		ModelAndView model = new ModelAndView();
-		model.addObject("title", "Spring Security Hello World");
-		model.addObject("message", "This is welcome page!");
-		model.setViewName("index");
-		return model;
- 
-	}
- 
 }
