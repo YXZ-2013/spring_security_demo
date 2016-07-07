2684543e465a772435e66ecd5d6b4b78b2cde56a
yinxunzhi <yinxunzhi@che001.com>
2016-07-07 17:30:25 +0800

    自定义登录的配置实现

--- src/main/java/com/yin/myproject/security/controller/LoginController.java
+++ src/main/java/com/yin/myproject/security/controller/LoginController.java
@@ -1,14 +1,19 @@
 package com.yin.myproject.security.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String loginPage() {
+	@RequestMapping(value = "/login1", method = RequestMethod.GET)
+	public String loginPage(@RequestParam(value = "error", required = false) String error, Model model) {
+		if (error != null) {
+			model.addAttribute("error", "Invalid username and password!");
+		}
 		return "login";
 	}
 }
